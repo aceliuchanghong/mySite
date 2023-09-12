ef60720a26b235a8f1d344f4941ebd4a3f7f2a9c
Lawrence-AIP <liuch@allinfinance.com>
2023-09-12 09:57:23 +0800

    latest

--- src/main/java/com/liu/site/SiteApplication.java
+++ src/main/java/com/liu/site/SiteApplication.java
@@ -1,9 +1,11 @@
 package com.liu.site;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.liu.site.dao")
 public class SiteApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/liu/site/controller/StudentController.java
+++ src/main/java/com/liu/site/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.liu.site.pojo.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,11 +17,15 @@ public class StudentController {
     @Autowired
     private StudentMapper studentMapper;
 
-    @GetMapping("/student")
+    @RequestMapping("/student")
     public List query() {
         List<Student> list = studentMapper.selectList(null);
+        System.out.println(list);
         return list;
     }
+
+
+
 
     @PostMapping("/student")
     public String save(Student student) {
