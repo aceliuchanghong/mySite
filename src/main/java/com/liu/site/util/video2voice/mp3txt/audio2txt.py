85a6aa20165d804f7f548c554d19082b1281bc53
Lawrence-AIP <liuch@allinfinance.com>
2023-09-18 17:30:41 +0800

    最新

---
+++ src/main/java/com/liu/site/util/video2voice/mp3txt/audio2txt.py
@@ -0,0 +1,10 @@
+# https://blog.csdn.net/FrenzyTechAI/article/details/131259440
+import os
+import openai
+
+def solution
+openai.api_key = os.getenv("OPENAI_API_KEY")
+audio_file= open("./data/generative_ai_topics_clip.mp3", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file, response_format="srt",
+                                     prompt="这是一段Onboard播客，里面会聊到PALM这个大语言模型。这个模型也叫做Pathways Language Model。")
+print(transcript)
