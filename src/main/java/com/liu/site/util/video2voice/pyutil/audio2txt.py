3b0e7582c656d7c1b0f49f8cf0f11ab1c15375df
Lawrence-AIP <liuch@allinfinance.com>
2023-09-22 10:07:30 +0800

    最新

--- src/main/java/com/liu/site/util/video2voice/pyutil/audio2txt.py
+++ src/main/java/com/liu/site/util/video2voice/pyutil/audio2txt.py
@@ -5,9 +5,9 @@ import io
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
+
 def getSrtFile(audio_file, prompt, srcpath="../mp3/",
                despath="../srt/"):
-
     proxyHost = "127.0.0.1"
     proxyPort = 10809
     proxies = {
@@ -26,13 +26,14 @@ def getSrtFile(audio_file, prompt, srcpath="../mp3/",
         transcript = openai.Audio.transcribe("whisper-1", file, response_format="srt",
                                              prompt=prompt)
         with open(despath + audio_file + ".srt", 'w') as f:
+            print("Srt fileName is:" + audio_file + ".srt")
             f.write(transcript)
     except Exception as e:
         print("Input Error:", e)
 
-def getSrtFileWithoutProxy(audio_file, prompt, srcpath="../mp3/",
-               despath="../srt/"):
 
+def getSrtFileWithoutProxy(audio_file, prompt, srcpath="../mp3/",
+                           despath="../srt/"):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     realFilePath = srcpath + audio_file
     try:
@@ -43,9 +44,11 @@ def getSrtFileWithoutProxy(audio_file, prompt, srcpath="../mp3/",
         transcript = openai.Audio.transcribe("whisper-1", file, response_format="srt",
                                              prompt=prompt)
         with open(despath + audio_file + ".srt", 'w') as f:
+            print("Srt fileName is:" + audio_file + ".srt")
             f.write(transcript)
     except Exception as e:
         print("Input Error:", e)
+
 
 # getSrtFile("saveHeart.mp3", "this is an audio about hearts which calling on people to protect their hearts")
 
@@ -55,9 +58,9 @@ srcpath = sys.argv[3]
 despath = sys.argv[4]
 if 3 > len(sys.argv) or 5 < len(sys.argv):
     print("请至少输入2个参数,最多4个:1.文件名字,2.prompt,3.srcpath,4.despath")
-elif 2 == len(sys.argv) - 1:
+elif 3 == len(sys.argv):
     getSrtFile(fileName, prompt)
-elif 3 == len(sys.argv) - 1:
+elif 4 == len(sys.argv):
     getSrtFile(fileName, prompt, srcpath)
-elif 4 == len(sys.argv) - 1:
+elif 5 == len(sys.argv):
     getSrtFile(fileName, prompt, srcpath, despath)
